7b3874159a41ac921758f178b65ff4152a63c886
Sodium-Man <bhalaninaman7@gmail.com>
2025-04-25 14:37:32 +0530

    bug fix

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,61 +1,57 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:image_picker/image_picker.dart';
 
 class AuthService {
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final FirebaseStorage _storage = FirebaseStorage.instance;
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+  final _storage = FirebaseStorage.instance;
 
-  Future<User?> signIn(String email, String password) async {
-    try {
-      UserCredential result = await _auth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return result.user;
-    } on FirebaseAuthException catch (e) {
-      throw FirebaseAuthException(code: e.code, message: e.message);
-    }
+  /// Stream of auth changes
+  Stream<User?> get authStateChanges => _auth.authStateChanges();
+
+  /// Sign In
+  Future<UserCredential> signIn(String email, String password) {
+    return _auth.signInWithEmailAndPassword(email: email, password: password);
   }
 
-  Future<User?> signUp(String email, String password) async {
-    try {
-      UserCredential result = await _auth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return result.user;
-    } on FirebaseAuthException catch (e) {
-      throw FirebaseAuthException(code: e.code, message: e.message);
-    }
-  }
-
-  Future<void> updateProfile({
-    required String uid,
+  /// Sign Up + profile creation
+  Future<void> signUp({
     required String name,
     required DateTime dob,
-    XFile? profileImage,
+    required String email,
+    required String password,
+    required File profileImage,
   }) async {
-    try {
-      String? imageUrl;
-      if (profileImage != null) {
-        Reference ref = _storage.ref().child('profile_images/$uid');
-        await ref.putData(await profileImage.readAsBytes());
-        imageUrl = await ref.getDownloadURL();
-      }
+    // 1) Create user
+    final cred = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
 
-      await _firestore.collection('users').doc(uid).set({
-        'name': name,
-        'dob': dob,
-        'profileImage': imageUrl,
-        'createdAt': FieldValue.serverTimestamp(),
-      }, SetOptions(merge: true));
-    } catch (e) {
-      throw Exception('Failed to update profile: $e');
-    }
+    final uid = cred.user!.uid;
+
+    // 2) Upload profile image
+    final ref = _storage.ref().child('profile_pictures/$uid.jpg');
+    await ref.putFile(profileImage);
+    final photoUrl = await ref.getDownloadURL();
+
+    // 3) Save extra fields in Firestore
+    await _firestore.collection('users').doc(uid).set({
+      'name': name,
+      'dob': dob.toIso8601String(),
+      'email': email,
+      'photoUrl': photoUrl,
+    });
   }
 
-  Future<void> signOut() async => await _auth.signOut();
+  /// Fetch additional profile (e.g. name) after login
+  Future<DocumentSnapshot<Map<String, dynamic>>> getProfile(String uid) {
+    return _firestore.collection('users').doc(uid).get();
+  }
+
+  /// Sign out
+  Future<void> signOut() => _auth.signOut();
 }
